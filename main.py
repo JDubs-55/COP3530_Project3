73fd38678bafbefb2e8404f16ef9a49fa63fa7d1
Wali Farooqi <96837843+Walifarooqi@users.noreply.github.com>
2022-12-05 21:02:09 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,248 +1,80 @@
-
-
 import pandas as pd
+import neo4j
+import py2neo
 import numpy as np
-import matplotlib.pyplot as plt
-import oracledb as db
+from gi.overrides import Gtk, Gdk, GObject, GLib
+from graph_tool.all import *
 
-import os
+df = pd.read_csv('NewGraphData.csv')
+# print(df.head(5))
+R = Graph()
+R.vertex_properties['reporter'] = R.new_vertex_property("string")
+R.vertex_properties['partner'] = R.new_vertex_property("string")
+R.vertex_properties['product'] = R.new_vertex_property("string")
+R.vertex_properties['trade'] = R.new_vertex_property("string")
+R.vertex_properties['year1988'] = R.new_vertex_property("float")
+R.vertex_properties['year2013'] = R.new_vertex_property("float")
+R.vertex_properties['differences'] = R.new_vertex_property("float")
 
+reporter = R.add_vertex(10)
+for v in R.get_vertices()[0:10]:
+    R.vp['reporter'][v] = df['REPORTERNAME'][v]
 
-def main():
-    print("Hello World")
+partner = R.add_vertex(145)
+for v in R.get_vertices()[10:155]:
+    R.vp['partner'][v] = df['PARTNERNAME'][v]
 
+product = R.add_vertex(340)
+for v in R.get_vertices()[155:495]:
+    R.vp['product'][v] = df['PRODUCTDESCRIPTION'][v]
+    R.vp['trade'][v] = df['TRADEFLOW'][v]
 
-class Graph(object):
-    def __init__(self, graph_dict=None):
+year = R.add_vertex(62605)
+for v in R.get_vertices()[495:62600]:
+    R.vp['year1988'][v] = df['1988'][v]
+    R.vp['year2013'][v] = df['2013'][v]
 
-        if graph_dict is None:
-            graph_dict = {}
-        self.__graph_dict = graph_dict
+trade = R.add_vertex(1)
+for v in R.get_vertices()[62600:62601]:
+    R.vp['trade'][v] = df['TRADEFLOW'][v]
 
-    def vertices(self):
-        return list(self.__graph_dict.keys())
+R.vp['differences'][v] = df['2013'][v] - df['1988'][v]
 
-    def edges(self):
-        return self.__generate_edges()
+for s, t in zip(reporter, partner):
+    R.add_edge(s, t)
 
-    def add_vertex(self, vertex):
-        if vertex not in self.__graph_dict:
-            self.__graph_dict[vertex] = []
+for s, t in zip(product, partner):
+    R.add_edge(s, t)
 
-    def add_edge(self, edge):
-        edge = set(edge)
-        (vertex1, vertex2) = tuple(edge)
-        if vertex1 in self.__graph_dict:
-            self.__graph_dict[vertex1].append(vertex2)
-        else:
-            self.__graph_dict[vertex1] = [vertex2]
-
-    def __generate_edges(self):
-        edges = []
-        for vertex in self.__graph_dict:
-            for neighbour in self.__graph_dict[vertex]:
-                if {neighbour, vertex} not in edges:
-                    edges.append({vertex, neighbour})
-        return edges
-
-    def find_isolated_vertices(self):
-        graph = self.__graph_dict
-        isolated = []
-        for vertex in graph:
-            if not graph[vertex]:
-                isolated += [vertex]
-        return isolated
-
-    def __str__(self):
-        res = "vertices: "
-        for k in self.__graph_dict:
-            res += str(k) + " "
-        res += "    edges: "
-
-        for edge in self.__generate_edges():
-            res += str(edge) + " "
-        return res
-
-    def find_path(self, start_vertex, end_vertex, path=None):
-        graph = self.__graph_dict
-        if path is None:
-            path = []
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
-            return None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_path = self.find_path(vertex,
-                                               end_vertex,
-                                               path)
-                if extended_path:
-                    return extended_path
-        return None
-
-    def find_all_paths(self, start_vertex, end_vertex, path=None):
-        graph = self.__graph_dict
-        if path is None:
-            path = []
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return [path]
-        if start_vertex not in graph:
-            return []
-        paths = []
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_paths = self.find_all_paths(vertex,
-                                                     end_vertex,
-                                                     path)
-                for p in extended_paths:
-                    paths.append(p)
-        return paths
-
-    def find_shortest_path(self, start_vertex, end_vertex, path=None):
-        if path is None:
-            path = []
-        graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
-            return None
-        shortest = None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                newpath = self.find_shortest_path(vertex,
-                                                  end_vertex,
-                                                  path)
-                if newpath:
-                    if not shortest or len(newpath) < len(shortest):
-                        shortest = newpath
-        return shortest
-
-    def find_longest_path(self, start_vertex, end_vertex, path=None):
-        if path is None:
-            path = []
-        graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
-            return None
-        longest = None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                newpath = self.find_longest_path(vertex,
-                                                 end_vertex,
-                                                 path)
-                if newpath:
-                    if not longest or len(newpath) > len(longest):
-                        longest = newpath
-        return longest
-
-    def find_all_paths_with_length(self, start_vertex, end_vertex, path=None, length=0):
-        if path is None:
-            path = []
-        graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            if length == len(path):
-                return [path]
-            else:
-                return []
-        if start_vertex not in graph:
-            return []
-        paths = []
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_paths = self.find_all_paths_with_length(vertex,
-                                                                 end_vertex,
-                                                                 path, length)
-                for p in extended_paths:
-                    paths.append(p)
-        return paths
-
-    def find_all_paths_with_length_less_than(self, start_vertex, end_vertex, path=None, length=0):
-        if path is None:
-            path = []
-        graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            if length >= len(path):
-                return [path]
-            else:
-                return []
-        if start_vertex not in graph:
-            return []
-        paths = []
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_paths = self.find_all_paths_with_length_less_than(vertex,
-                                                                           end_vertex,
-                                                                           path, length)
-                for p in extended_paths:
-                    paths.append(p)
-        return paths
-
-    def getWeight(self, node):
-        return self.__graph_dict[node][0]
-
-    def __iter__(self):
-        self._iter_obj = iter(self._graph_dict)
-        return self._iter_obj
-
-    def __next__(self):
-        return next(self.__graph_dict)
-
-    def __getitem__(self, item):
-        return self.__graph_dict[item]
-
-    def __setitem__(self, key, value):
-        self.__graph_dict[key] = value
-
-    def __delitem__(self, key):
-        del self.__graph_dict[key]
-
-    def __len__(self):
-        return len(self.__graph_dict)
+for s, t in zip(year, product):
+    R.add_edge(s, t)
 
 
-class Edges:
-    def __init__(self, u, v, w):
-        self.__graph_dict = None
-        self.u = u
-        self.v = v
-        self.w = w
+for e in bfs_iterator(R):
+    print(reporter, product, year, partner)
+    print(e.source(), "->", e.target())
 
-    def __lt__(self, other):
-        return self.w < other.w
 
-    def __str__(self):
-        return str(self.u) + " - " + str(self.v) + " : " + str(self.w)
 
-    def __repr__(self):
-        return self.__str__()
+#Version 2
+edge_list = []
 
-    def __eq__(self, other):
-        return self.u == other.u and self.v == other.v and self.w == other.w
+g = load_graph_from_csv('NewGraphData.csv', directed=True, hashed=True, csv_options={"delimiter": ","},
+                        ecols=[1, 3, 4, 6, 7, 8, 9])
 
-    def __hash__(self):
-        return hash(self.__str__())
+g.vertex_properties['reporter'] = g.new_vertex_property("string")
+g.vertex_properties['partner'] = g.new_vertex_property("string")
+g.vertex_properties['product'] = g.new_vertex_property("string")
+g.vertex_properties['trade'] = g.new_vertex_property("string")
+g.vertex_properties['year1988'] = g.new_vertex_property("float")
+g.vertex_properties['year2013'] = g.new_vertex_property("float")
+g.vertex_properties['differences'] = g.new_vertex_property("float")
 
-    def find_path(self, start_vertex, end_vertex, path=None):
-        if path is None:
-            path = []
-        graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
-            return None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_path = self.find_path(vertex,
-                                               end_vertex,
-                                               path)
-                if extended_path:
-                    return extended_path
-        return None
+for e in bfs_iterator(g):
+    print(reporter, partner, product, year, trade)
+    print(e.source(), "->", e.target())
+
+
+graph_tool.draw.graph_draw(g, vertex_text=g.vp['reporter'], vertex_font_size=18, output_size=(2000, 2000), output="graph.png")
+graph_tool.draw.graph_draw(R, vertex_text=R.vp['reporter'], vertex_font_size=18, output_size=(2000, 2000), output="graphVERSION1.png")
+# props = g.add_edge_list(df[['1988', '2013', 'Difference']].values, hashed=True)
